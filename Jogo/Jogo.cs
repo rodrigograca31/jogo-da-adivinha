87f3608b52dae02f6aff7460b6fc20a26529795a
Rodrigo Graça <rodrigograca1993@hotmail.com>
2014-06-16 21:40:42 +0100

    Game class initial methods created

--- Jogo/Jogo.cs
+++ Jogo/Jogo.cs
@@ -8,6 +8,32 @@ namespace Jogo
 {
     public class Jogo
     {
+        private int limInferior;
+        private int limSuperior;
+        private int numeroSorteado;
+        private int numMaxTentativas;
+        private int numTentado;
+        private int numTentativas;
 
+        public Jogo(int limSuperior, int limInferior, int numMaxTentativas)
+        {
+            numeroSorteado = RandomNumber(1, 100);
+        }
+        private int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }        public String Ajuda(){
+            if (numTentado > numeroSorteado)
+            {
+                return "maior";
+            } else {
+                return "menor";
+            }
+        }        public Boolean PodeJogar(){
+            return (numTentativas < numMaxTentativas) ? true : false;
+        }        public Boolean Tentativa(int numero){
+            return (numero == numeroSorteado) ? true : false;
+        }
     }
 }
