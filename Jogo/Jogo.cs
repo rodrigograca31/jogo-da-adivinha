6aeb52c2f6544a168bab0bb8a7f0beb474e9ee02
Rodrigo Graça <rodrigograca1993@hotmail.com>
2014-06-16 22:07:09 +0100

    Version 1.0

--- Jogo/Jogo.cs
+++ Jogo/Jogo.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Jogo
+namespace JogoDaAdivinha
 {
     public class Jogo
     {
@@ -17,7 +17,8 @@ namespace Jogo
 
         public Jogo(int limSuperior, int limInferior, int numMaxTentativas)
         {
-            numeroSorteado = RandomNumber(1, 100);
+            this.numeroSorteado = RandomNumber(limInferior, limSuperior);
+            this.numMaxTentativas = numMaxTentativas;
         }
         private int RandomNumber(int min, int max)
         {
@@ -34,6 +35,61 @@ namespace Jogo
             return (numTentativas < numMaxTentativas) ? true : false;
         }        public Boolean Tentativa(int numero){
             return (numero == numeroSorteado) ? true : false;
+        }
+
+        public int LimInferior{
+            set
+            {
+                this.limInferior = value;
+            }
+            get
+            {
+                return limInferior;
+            }
+        }
+        public int LimSuperior
+        {
+            set
+            {
+                this.limSuperior = value;
+            }
+            get
+            {
+                return limSuperior;
+            }
+        }
+        public int NumMaxTentativas
+        {
+            set
+            {
+                this.numMaxTentativas = value;
+            }
+            get
+            {
+                return numMaxTentativas;
+            }
+        }
+        public int NumTentado
+        {
+            set
+            {
+                this.numTentado = value;
+            }
+            get
+            {
+                return numTentado;
+            }
+        }
+        public int NumTentativas
+        {
+            set
+            {
+                this.numTentativas = value;
+            }
+            get
+            {
+                return numTentativas;
+            }
         }
     }
 }
---
+++ JogoDaAdivinha/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace JogoDaAdivinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limInf = 0;
+            int limSup = 5;
+            int numTent = 3;
+            int meuNumero = 0;
+
+            Jogo j = new Jogo(limSup, limInf, numTent);
+
+            Console.WriteLine("Acabei de pensar num número entre {0} e {1}.\nConsegue adivinhá-lo em {2} tentativas?", limInf, limSup, numTent);
+
+            while (j.PodeJogar())
+            {
+                try
+                {
+                    meuNumero = int.Parse(Console.ReadLine());
+
+                    if (j.Tentativa(meuNumero) == true)
+                    {
+                        Console.WriteLine("Parabéns!\nAcertou em cheio!");
+                        Console.Read();
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("O seu número é {0} que o meu.", j.Ajuda());
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Cuidado, deve inserir um número!");
+                }
+
+            }
+            Console.WriteLine("Dogh!");
+        }
+    }
+}
